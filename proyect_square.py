b5c96ec7f172b64c038f85febf03e4c5c510fca0
murderouz <162519447+murderouz@users.noreply.github.com>
2024-04-16 20:26:36 -0500

    Add files via upload

---
+++ proyect_square.py
@@ -0,0 +1,16 @@
+# Solicitar la Longitud
+
+parametro = int(input("Cual es la longitud del cuadrado?\n"))
+area = parametro * parametro
+
+# Calcular el area 
+
+def area_square():
+    return area
+
+perimetro = parametro * 4
+
+# Mostrar resultado
+
+print(f"El area del cuadrado es de {area_square()}m2")
+print(f"el perimetro del cuadrado es {perimetro}m")
